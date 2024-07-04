03c8c816bdf7a2faa1c670959ce2cb654caf8680
karizacharlotte <c.kariza@alustudent.com>
2024-07-04 09:41:02 +0000

    added a file

--- alu-scripting/api_advanced/0-subs.py
+++ alu-scripting/api_advanced/0-subs.py
@@ -1,21 +1,22 @@
-#!/usr/bin/python3
-"""get subscribers numbers function"""
 
-
-import json
-import requests
-import sys
-
+        return body["data"]["subscribers"]
+rt requests
 
 def number_of_subscribers(subreddit):
-    """get all subscribers"""
-    if len(sys.argv) < 2:
-        return 0
-    else:
-        url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-        headers = {"User-Agent": "Mozilla/5.0"}
-        result = requests.get(url, headers=headers, allow_redirects=False)
-        if result.status_code != 200:
+    """
+    Queries the Reddit API and returns the number of subscribers for a given subreddit.
+    If an invalid subreddit is given, returns 0.
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {"User-Agent": "custom-user-agent"}
+
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code == 200:
+            data = response.json()
+            return data['data']['subscribers']
+        else:
             return 0
-        body = json.loads(result.text)
-        return body["data"]["subscribers"]
+    except requests.RequestException:
+        return 0
+

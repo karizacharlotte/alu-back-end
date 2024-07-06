79d779dd1bee2893ce474cd0fefebb0f996c4a31
karizacharlotte <c.kariza@alustudent.com>
2024-07-06 09:30:56 +0000

    x

--- alu-scripting/api_advanced/0-subs.py
+++ alu-scripting/api_advanced/0-subs.py
@@ -1,28 +1,22 @@
-mport requests
+#!/usr/bin/python3
+"""
+ returns the number of subscribers if
+ the request is successful.
+ returns 0 if the subreddit is not found (HTTP 404)
+"""
+import requests
+
 
 def number_of_subscribers(subreddit):
     """
-    Queries the Reddit API and returns the number of subscribers for a given subreddit.
-    If the subreddit is invalid, returns 0.
+    Fetches the number of subscribers for a given subreddit
     """
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {"User-Agent": "subreddit-subscriber-counter/0.1"}
-    
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        
-        if response.status_code == 200:
-            data = response.json()
-            return data['data']['subscribers']
-        else:
-            return 0
-    except requests.RequestException:
+    user_agent = {'User-agent': 'rodwol'}
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+    response = requests.get(url, headers=user_agent, allow_redirects=False)
+
+    if response.status_code == 200:
+        subscribers = response.json()
+        return subscribers['data']['subscribers']
+    elif response.status_code == 404:
         return 0
-
-if __name__ == '__main__':
-    import sys
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
-    else:
-        print("{:d}".format(number_of_subscribers(sys.argv[1])))
-

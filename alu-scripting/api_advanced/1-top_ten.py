ac91e4afc66224f803e5137a898a521a5d88e75b
karizacharlotte <c.kariza@alustudent.com>
2024-07-06 09:06:06 +0000

    eddited a file

--- alu-scripting/api_advanced/1-top_ten.py
+++ alu-scripting/api_advanced/1-top_ten.py
@@ -1,32 +1,19 @@
+#!/usr/bin/python3
+"""
+function that queries the 'Reddit API'
+and prints the titles of the first 10 hot posts listed for a given subreddit.
+"""
+import requests
 
-                print(post["data"]["title"])
-rt requests
 
 def top_ten(subreddit):
-    """
-    Queries the Reddit API and prints the titles of the first 10 hot posts listed for a given subreddit.
-    If the subreddit is invalid, prints None.
-    """
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
-    headers = {"User-Agent": "subreddit-hot-posts/0.1"}
-    
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        
-        if response.status_code == 200:
-            data = response.json()
-            posts = data['data']['children']
-            for post in posts:
-                print(post['data']['title'])
-        else:
-            print(None)
-    except requests.RequestException:
+    """ prints the titles of the first 10 hot posts listed in a subreddit """
+    url = 'https://oauth.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code != 200:
         print(None)
-
-if __name__ == '__main__':
-    import sys
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
-    else:
-        top_ten(sys.argv[1])
-
+        return
+    posts = response.json()['data']['children']
+    for post in posts:
+        print(post['data']['title'])
